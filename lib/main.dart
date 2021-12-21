5318eb115e08c15d730bb53f50466cff1fbcf2d0
SatisKia <satis@msd.biglobe.ne.jp>
2021-12-22 07:36:52 +0900

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 
-// ラジオボタンの値
-enum RadioValue { hoge, fuga, piyo }
-
 void main() {
   runApp(const MyApp());
 }
@@ -29,13 +26,28 @@ class _MyHomePageState extends State {
   double contentWidth  = 0.0;
   double contentHeight = 0.0;
 
-  double bottomSpace = 0.0;
+  ScrollController? scrollController;
+
+  @override
+  void initState(){
+    super.initState();
+    scrollController = ScrollController();
+    WidgetsBinding.instance!.addPostFrameCallback((_) {
+      scrollController!.jumpTo( scrollController!.position.maxScrollExtent );
+    });
+  }
+
+  @override
+  void dispose() {
+    scrollController!.dispose();
+    super.dispose();
+  }
 
   // チェックボックス
-  bool _checkBoxValue = true;
-  void _onCheckBoxChanged(bool? value) {
+  bool _checkboxValue = true;
+  void _onCheckboxChanged(bool? value) {
     setState(() {
-      _checkBoxValue = value!;
+      _checkboxValue = value!;
     });
   }
 
@@ -48,10 +60,16 @@ class _MyHomePageState extends State {
   }
 
   // ラジオボタン
-  RadioValue _radioValue = RadioValue.hoge;
-  void _onRadioChanged(RadioValue? value) {
+  int _radio1Value = 0;
+  void _onRadio1Changed(int? value) {
     setState(() {
-      _radioValue = value!;
+      _radio1Value = value!;
+    });
+  }
+  int _radio2Value = 0;
+  void _onRadio2Changed(int? value) {
+    setState(() {
+      _radio2Value = value!;
     });
   }
 
@@ -118,7 +136,294 @@ class _MyHomePageState extends State {
     contentWidth  = MediaQuery.of( context ).size.width;
     contentHeight = MediaQuery.of( context ).size.height - MediaQuery.of( context ).padding.top;
 
-    bottomSpace = MediaQuery.of( context ).viewInsets.bottom;
+    bool buttonTest    = true;
+    bool checkboxTest  = true;
+    bool switchTest    = true;
+    bool radioTest     = true;
+    bool textFieldTest = true;
+    bool dropdownTest  = true;
+    var body = <Widget>[];
+    body.add(
+        const SizedBox(height: 10)
+    );
+
+    // ボタン
+    if( buttonTest ) {
+      body.add( Column(children: [
+        Row(children: [
+          TextButton(
+            onPressed: () {},
+            child: const Text('TextButton'),
+          ),
+          const SizedBox(width: 10),
+          TextButton(
+            style: TextButton.styleFrom(
+              primary: Colors.red,
+            ),
+            onPressed: () {},
+            child: const Text('TextButton'),
+          ),
+          const SizedBox(width: 10),
+          const TextButton(
+            onPressed: null,
+            child: Text('TextButton'),
+          )
+        ] ),
+        Row(children: [
+          OutlinedButton(
+            onPressed: () {},
+            child: const Text('Outlined'),
+          ),
+          const SizedBox(width: 10),
+          OutlinedButton(
+            style: OutlinedButton.styleFrom(
+                primary: Colors.red,
+                backgroundColor: Colors.yellow
+            ),
+            onPressed: () {},
+            child: const Text('Outlined'),
+          ),
+          const SizedBox(width: 10),
+          const OutlinedButton(
+            onPressed: null,
+            child: Text('Outlined'),
+          )
+        ] ),
+        Row(children: [
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              elevation: 5,
+            ),
+            onPressed: () {},
+            child: const Text('Elevated'),
+          ),
+          const SizedBox(width: 10),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              primary: Colors.yellow,
+              onPrimary: Colors.red,
+              shadowColor: Colors.orange,
+              elevation: 5,
+            ),
+            onPressed: () {},
+            child: const Text('Elevated'),
+          ),
+          const SizedBox(width: 10),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              elevation: 5,
+            ),
+            onPressed: null,
+            child: const Text('Elevated'),
+          )
+        ] )
+      ] ) );
+    }
+
+    // チェックボックス
+    if( checkboxTest ) {
+      body.add(
+          CheckboxListTile(
+              title: const Text('チェックボックス'),
+              value: _checkboxValue,
+              controlAffinity: ListTileControlAffinity.leading,
+              onChanged: _onCheckboxChanged
+          )
+      );
+    }
+
+    // スイッチ
+    if( switchTest ) {
+      body.add(
+          SwitchListTile(
+              title: const Text('スイッチ'),
+              value: _switchValue,
+              controlAffinity: ListTileControlAffinity.trailing,
+              onChanged: _onSwitchChanged
+          )
+      );
+    }
+
+    // ラジオボタン
+    if( radioTest ) {
+      body.add( Column(children: [
+        RadioListTile(
+            title: const Text('ラジオボタン1'),
+            value: 0,
+            groupValue: _radio1Value,
+            controlAffinity: ListTileControlAffinity.leading,
+            onChanged: _onRadio1Changed
+        ),
+        RadioListTile(
+            title: const Text('ラジオボタン2'),
+            value: 1,
+            groupValue: _radio1Value,
+            controlAffinity: ListTileControlAffinity.leading,
+            onChanged: _onRadio1Changed
+        ),
+        RadioListTile(
+            title: const Text('ラジオボタン3'),
+            value: 2,
+            groupValue: _radio1Value,
+            controlAffinity: ListTileControlAffinity.leading,
+            onChanged: _onRadio1Changed
+        ),
+        Row(children: [
+          Container(width: contentWidth / 3, height: 64, alignment: Alignment.center,
+            child: RadioListTile(
+                title: Row(children: [ SizedBox(
+                    width: 32,
+                    height: 32,
+                    child: Image.asset('assets/icon1.png', fit: BoxFit.fill)
+                ) ] ),
+                value: 0,
+                groupValue: _radio2Value,
+                controlAffinity: ListTileControlAffinity.leading,
+                onChanged: _onRadio2Changed
+            ),
+          ),
+          Container(width: contentWidth / 3, height: 64, alignment: Alignment.center,
+            child: RadioListTile(
+                title: Row(children: [ SizedBox(
+                    width: 32,
+                    height: 32,
+                    child: Image.asset('assets/icon4.png', fit: BoxFit.fill)
+                ) ] ),
+                value: 1,
+                groupValue: _radio2Value,
+                controlAffinity: ListTileControlAffinity.leading,
+                onChanged: _onRadio2Changed
+            ),
+          ),
+          Container(width: contentWidth / 3, height: 64, alignment: Alignment.center,
+            child: RadioListTile(
+                title: Row(children: [ SizedBox(
+                    width: 32,
+                    height: 32,
+                    child: Image.asset('assets/icon5.png', fit: BoxFit.fill)
+                ) ] ),
+                value: 2,
+                groupValue: _radio2Value,
+                controlAffinity: ListTileControlAffinity.leading,
+                onChanged: _onRadio2Changed
+            ),
+          ),
+        ] )
+      ] ) );
+    }
+
+    // テキスト入力
+    if( textFieldTest ) {
+      body.add( Column(children: [
+        TextField(
+          decoration: InputDecoration(
+            border: const OutlineInputBorder(),
+            contentPadding: EdgeInsets.only(left: contentWidth / 40,
+                top: 0.0,
+                right: contentWidth / 40,
+                bottom: 0.0),
+            hintText: 'テキストを入力',
+          ),
+          autocorrect: false,
+          keyboardType: TextInputType.text,
+          onChanged: _text1Changed,
+          onSubmitted: _text1Submitted,
+        ),
+        Row(children: [
+          Padding(padding: EdgeInsets.only(
+              top: contentWidth / 80, bottom: contentWidth / 80),
+              child: Text("入力中：$_changeText1",
+                style: const TextStyle(color: Colors.grey),))
+        ] ),
+        Row(children: [
+          Padding(padding: EdgeInsets.only(
+              top: contentWidth / 80, bottom: contentWidth / 80),
+              child: Text("入力決定：$_submitText1",
+                style: const TextStyle(color: Colors.grey),))
+        ] ),
+        const SizedBox(height: 10),
+        TextField(
+          decoration: InputDecoration(
+            border: const OutlineInputBorder(),
+            contentPadding: EdgeInsets.only(left: contentWidth / 40,
+                top: 0.0,
+                right: contentWidth / 40,
+                bottom: 0.0),
+            hintText: '英数字を入力',
+          ),
+          autocorrect: false,
+          keyboardType: TextInputType.number,
+          onChanged: _text2Changed,
+          onSubmitted: _text2Submitted,
+        ),
+        Row(children: [
+          Padding(padding: EdgeInsets.only(
+              top: contentWidth / 80, bottom: contentWidth / 80),
+              child: Text("入力中：$_changeText2",
+                style: const TextStyle(color: Colors.grey),))
+        ] ),
+        Row(children: [
+          Padding(padding: EdgeInsets.only(
+              top: contentWidth / 80, bottom: contentWidth / 80),
+              child: Text("入力決定：$_submitText2",
+                style: const TextStyle(color: Colors.grey),))
+        ] ),
+        const SizedBox(height: 10),
+        TextField(
+          decoration: InputDecoration(
+            border: const OutlineInputBorder(),
+            contentPadding: EdgeInsets.only(left: contentWidth / 40,
+                top: 0.0,
+                right: contentWidth / 40,
+                bottom: 0.0),
+            hintText: '伏せ字英数字を入力',
+          ),
+          autocorrect: false,
+          keyboardType: TextInputType.number,
+          obscureText: true,
+          onChanged: _text3Changed,
+          onSubmitted: _text3Submitted,
+        ),
+        Row(children: [
+          Padding(padding: EdgeInsets.only(
+              top: contentWidth / 80, bottom: contentWidth / 80),
+              child: Text("入力中：$_changeText3",
+                style: const TextStyle(color: Colors.grey),))
+        ] ),
+        Row(children: [
+          Padding(padding: EdgeInsets.only(
+              top: contentWidth / 80, bottom: contentWidth / 80),
+              child: Text("入力決定：$_submitText3",
+                style: const TextStyle(color: Colors.grey),))
+        ] ),
+        const SizedBox(height: 10),
+      ] ) );
+    }
+
+    // ドロップダウンメニュー
+    if( dropdownTest ) {
+      body.add( Row( children: [
+        SizedBox( width: contentWidth * 0.7, height: contentWidth / 8,
+          child: DropdownButtonFormField(
+            decoration: InputDecoration(
+              contentPadding: EdgeInsets.only(left: contentWidth / 40,
+                  top: 0.0,
+                  right: contentWidth / 40,
+                  bottom: 0.0),
+              enabledBorder: const OutlineInputBorder(),
+            ),
+            items: _dropdownMenu.entries.map((entry) {
+              return DropdownMenuItem(
+                child: Text(entry.value),
+                value: entry.key,
+              );
+            }).toList(),
+            value: _dropdownValue,
+            onChanged: _onDropdownChanged,
+          ),
+        ),
+      ] ) );
+    }
 
     return Scaffold(
         appBar: AppBar(
@@ -126,113 +431,13 @@ class _MyHomePageState extends State {
         ),
         resizeToAvoidBottomInset: false, // 自前で高さ対応する
         body: SingleChildScrollView(
+            controller: scrollController,
             reverse: true, // スクロールの向きを逆にする
             child: Padding(
-              padding: EdgeInsets.only(bottom: bottomSpace),
-              child: Column( children: [
-                // チェックボックス
-                CheckboxListTile(
-                    title: const Text('チェックボックス'),
-                    value: _checkBoxValue,
-                    controlAffinity: ListTileControlAffinity.leading,
-                    onChanged: _onCheckBoxChanged
-                ),
-
-                // スイッチ
-                SwitchListTile(
-                    title: const Text('スイッチ'),
-                    value: _switchValue,
-                    controlAffinity: ListTileControlAffinity.trailing,
-                    onChanged: _onSwitchChanged
-                ),
-
-                // ラジオボタン
-                RadioListTile(
-                    title: const Text('ラジオボタン1'),
-                    value: RadioValue.hoge,
-                    groupValue: _radioValue,
-                    controlAffinity: ListTileControlAffinity.leading,
-                    onChanged: _onRadioChanged
-                ),
-                RadioListTile(
-                    title: const Text('ラジオボタン2'),
-                    value: RadioValue.fuga,
-                    groupValue: _radioValue,
-                    controlAffinity: ListTileControlAffinity.leading,
-                    onChanged: _onRadioChanged
-                ),
-                RadioListTile(
-                    title: const Text('ラジオボタン3'),
-                    value: RadioValue.piyo,
-                    groupValue: _radioValue,
-                    controlAffinity: ListTileControlAffinity.leading,
-                    onChanged: _onRadioChanged
-                ),
-
-                // テキスト入力
-                TextField(
-                  decoration: InputDecoration(
-                    border: const OutlineInputBorder(),
-                    contentPadding: EdgeInsets.only(left: contentWidth / 40, top: 0.0, right: contentWidth / 40, bottom: 0.0),
-                    hintText: 'テキストを入力',
-                  ),
-                  autocorrect: false,
-                  keyboardType: TextInputType.text,
-                  onChanged: _text1Changed,
-                  onSubmitted: _text1Submitted,
-                ),
-                Row( children: [ Padding( padding: EdgeInsets.only(top: contentWidth / 80, bottom: contentWidth / 80), child: Text( "入力中：$_changeText1", style: const TextStyle( color: Color(0xFFFF0000)), ) ) ] ),
-                Row( children: [ Padding( padding: EdgeInsets.only(top: contentWidth / 80, bottom: contentWidth / 80), child: Text( "入力決定：$_submitText1", style: const TextStyle( color: Color(0xFFFF0000)), ) ) ] ),
-                TextField(
-                  decoration: InputDecoration(
-                    border: const OutlineInputBorder(),
-                    contentPadding: EdgeInsets.only(left: contentWidth / 40, top: 0.0, right: contentWidth / 40, bottom: 0.0),
-                    hintText: '英数字を入力',
-                  ),
-                  autocorrect: false,
-                  keyboardType: TextInputType.number,
-                  onChanged: _text2Changed,
-                  onSubmitted: _text2Submitted,
-                ),
-                Row( children: [ Padding( padding: EdgeInsets.only(top: contentWidth / 80, bottom: contentWidth / 80), child: Text( "入力中：$_changeText2", style: const TextStyle( color: Color(0xFFFF0000)), ) ) ] ),
-                Row( children: [ Padding( padding: EdgeInsets.only(top: contentWidth / 80, bottom: contentWidth / 80), child: Text( "入力決定：$_submitText2", style: const TextStyle( color: Color(0xFFFF0000)), ) ) ] ),
-                TextField(
-                  decoration: InputDecoration(
-                    border: const OutlineInputBorder(),
-                    contentPadding: EdgeInsets.only(left: contentWidth / 40, top: 0.0, right: contentWidth / 40, bottom: 0.0),
-                    hintText: '伏せ字英数字を入力',
-                  ),
-                  autocorrect: false,
-                  keyboardType: TextInputType.number,
-                  obscureText: true,
-                  onChanged: _text3Changed,
-                  onSubmitted: _text3Submitted,
-                ),
-                Row( children: [ Padding( padding: EdgeInsets.only(top: contentWidth / 80, bottom: contentWidth / 80), child: Text( "入力中：$_changeText3", style: const TextStyle( color: Color(0xFFFF0000)), ) ) ] ),
-                Row( children: [ Padding( padding: EdgeInsets.only(top: contentWidth / 80, bottom: contentWidth / 80), child: Text( "入力決定：$_submitText3", style: const TextStyle( color: Color(0xFFFF0000)), ) ) ] ),
-
-                Row( children: [
-                  // ドロップダウンメニュー
-                  SizedBox(
-                    width: contentWidth * 0.7,
-                    height: contentWidth / 8,
-                    child: DropdownButtonFormField(
-                      decoration: InputDecoration(
-                        contentPadding: EdgeInsets.only(left: contentWidth / 40, top: 0.0, right: contentWidth / 40, bottom: 0.0),
-                        enabledBorder: const OutlineInputBorder(),
-                      ),
-                      items: _dropdownMenu.entries.map((entry) {
-                        return DropdownMenuItem(
-                          child: Text(entry.value),
-                          value: entry.key,
-                        );
-                      }).toList(),
-                      value: _dropdownValue,
-                      onChanged: _onDropdownChanged,
-                    ),
-                  ),
-                ] ),
-              ] ),
+              padding: EdgeInsets.only(
+                bottom: MediaQuery.of( context ).viewInsets.bottom,
+              ),
+              child: Column( children: body ),
             )
         )
     );
